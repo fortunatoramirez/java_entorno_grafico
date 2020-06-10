c85f1315ac8fc38f9da823d2f2b35db3b1b90fbb
Fortunato <fortunato.ramirez@tectijuana.edu.mx>
2020-06-10 12:27:47 -0700

    Entorno gráfico simple. Lanzando solamente una ventana.

---
+++ MiVentanaPrincipal.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+class MiVentanaPrincipal extends JFrame{
+
+	public MiVentanaPrincipal()
+	{
+		this.setBounds(400,400,500,350);
+		this.setTitle("Ventana Principal");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
